187d3359a23a86e3f80277e97e9a5fc517d0c04e
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-08-30 00:33:14 +0100

    Style Font Form

--- Template/config/fonts.php
+++ Template/config/fonts.php
@@ -5,7 +5,7 @@
     <p class="fonts-page-intro">
         <?= t('By default, the application uses specific fonts with fallbacks depending on the operating system. Switch to one of the fonts below to change the text style across the application. All fonts listed here are open source webfonts. These fonts are included with this plugin but only used if selected. Select from one of the fonts below to override the default fonts.') ?>
     </p>
-    <form class="font-form panel" method="post" action="<?= $this->url->href('FontSwitcherController', 'save', array('redirect' => 'showFonts', 'plugin' => 'FontSwitcher')) ?>" autocomplete="off">
+    <form id="FontForm" class="font-form panel" method="post" action="<?= $this->url->href('FontSwitcherController', 'save', array('redirect' => 'showFonts', 'plugin' => 'FontSwitcher')) ?>" autocomplete="off">
         <h3 class=""><?= t('Font Options') ?></h3>
         <?= $this->form->csrf() ?>
         <div class="form-actions">
