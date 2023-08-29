0253806cbf4556b15fa1c27b764b4dc0b9eea35a
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-08-29 02:09:04 +0100

    Style Font Settings Section

--- Template/config/font-settings.php
+++ Template/config/font-settings.php
@@ -1,5 +1,8 @@
-<fieldset id="" class="panel">
-    <h3 class="">FontSwitcher</h3>
+<fieldset id="FontSettingsSection" class="panel">
+    <h3 class="">
+        <span class="font-switcher-icon"></span>
+        <span class="v-middle"><?= t('Font Switcher') ?></span>
+    </h3>
     <p class="">
         <?= e('The site font can be changed in the %s page.', $this->url->link(t('Font Settings'), 'FontSwitcherController', 'showFonts', ['plugin' => 'FontSwitcher'], false, 'font-page-link', t('Visit page'), false, '')) ?>
     </p>
