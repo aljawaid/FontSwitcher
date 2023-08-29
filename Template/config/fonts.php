97440d0eefa5044c75b3330ce2206d350b560831
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-08-30 00:34:03 +0100

    Add Default Fonts Section

--- Template/config/fonts.php
+++ Template/config/fonts.php
@@ -8,6 +8,33 @@
     <form id="FontForm" class="font-form panel" method="post" action="<?= $this->url->href('FontSwitcherController', 'save', array('redirect' => 'showFonts', 'plugin' => 'FontSwitcher')) ?>" autocomplete="off">
         <h3 class=""><?= t('Font Options') ?></h3>
         <?= $this->form->csrf() ?>
+
+        <!-- Default Fonts -->
+        <fieldset class="<?= ($this->task->configModel->get('font_switcher_font_name') == 'default_fonts') ? 'font-wrapper-enabled' : 'font-wrapper'?>">
+            <legend class=""><?= t('Default Fonts') ?></legend>
+            <span class="default-files-icon"></span>
+            <span class="font-typeface" title="<?= t('Font Typeface') ?>">sans-serif</span>
+            <div class="font-option-wrapper">
+                <?= $this->form->radio('font_switcher_font_name', t('Use default fonts'), 'default_fonts', true, isset($values['font_switcher_font_name']) && $values['font_switcher_font_name'] == 'default_fonts') ?>
+            </div>
+            <div class="font-description-wrapper">
+                <ul class="font-description">
+                    <li class="font-description-name"><?= e('Font Names with Fallbacks %s', '<i>' . t('(in order)') . '</i>') ?></li>
+                    <li class="font-name-value">Helvetica Neue, Helvetica, Arial, sans-serif</li>
+                </ul>
+                <ul class="font-description-styles">
+                    <li class="font-description-styles-title"><?= t('Styles') ?></li>
+                    <li class="font-style-value">Regular (400 normal)</li>
+                    <li class="font-style-value">Italic</li>
+                    <li class="font-style-value">Bold (700)</li>
+                    <li class="font-style-value">Bold Italic (700 italic)</li>
+                </ul>
+                <ul class="font-description-charsets">
+                    <li class="font-description-charsets-title"><?= t('Character Sets') ?></li>
+                    <li class="font-charset-value" title="latin">Latin</li>
+                </ul>
+            </div>
+        </fieldset>
         <div class="form-actions">
             <button type="submit" class="btn btn-blue"><?= t('Save Font Settings') ?></button>
             <a href="<?= $this->url->href('ConfigController', 'application', array(), false, 'FontSettingsSection') ?>" class="btn">
