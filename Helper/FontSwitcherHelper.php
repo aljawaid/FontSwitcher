2bdc033ed00b2e3a6e7b68aa4f24434f0b92f43a
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-09-02 22:00:31 +0100

    Add FontSwitcherHelper.php

---
+++ Helper/FontSwitcherHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Kanboard\Plugin\FontSwitcher\Helper;
+
+use Kanboard\Core\Base;
+
+/**
+ * FontSwitcher Helper
+ *
+ * @package  Helper
+ * @see 
+ * @author   aljawaid
+ */
+class FontSwitcherHelper extends Base
+{
+    /**
+     * Get the User Font Preference
+     *
+     * @return void
+     * @author 
+     */
+    function getFontPreference()
+    {
+        return true;
+    }
+}
